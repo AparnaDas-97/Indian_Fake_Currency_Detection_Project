4e67caec652f510532dd743e81b45708dc27df82
Aparna Das <aparnakrishnadas97@gmail.com>
2021-05-06 14:04:23 +0530

    First Commit

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cv2
+import matplotlib.pyplot as plt
+
+# reading a image
+img = cv2.imread('2000.jpg')
+img = cv2.imread('2000.jpg')
+imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgCanny = cv2.Canny(img, 100, 100)
+orb = cv2.ORB_create()  # OpenCV 3 backward incompatibility: Do not create a detector with `cv2.ORB()`.
+key_points, description = orb.detectAndCompute(imgCanny, None)
+img_building_keypoints = cv2.drawKeypoints(imgCanny,
+                                           key_points,
+                                           imgCanny,
+                                           flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)  # Draw circles.
+plt.figure(figsize=(16, 16))
+plt.title('ORB Interest Points')
+plt.imshow(img_building_keypoints)
+plt.show()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def image_detect_and_compute(detector, img_name):
+    img = cv2.imread(img_name)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    kp, des = detector.detectAndCompute(img, None)
+    return img, kp, des
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def draw_image_matches(detector, img1_name, img2_name, nmatches=10):
+    img1, kp1, des1 = image_detect_and_compute(detector, img1_name)
+    img2, kp2, des2 = image_detect_and_compute(detector, img2_name)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(des1, des2)
+    matches = sorted(matches, key=lambda x: x.distance)  # Sort matches by distance.  Best come first.
+
+    img_matches = cv2.drawMatches(img1, kp1, img2, kp2, matches[:nmatches], img2, flags=2)  # Show top 10 matches
+    plt.figure(figsize=(16, 16))
+    plt.title(type(detector))
+    plt.imshow(img_matches)
+    plt.show()
+
+
+orb = cv2.ORB_create()
+draw_image_matches(orb, '2000.jpg', '200f.jpg')
+
+
+
+
+
